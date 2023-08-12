e8434ce187adc19258ab4f33f66e54e730fd48d3
Soul-8789 <102282026+Soul-8789@users.noreply.github.com>
2023-08-12 14:53:17 +0530

    second commit

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/Soul-8789/portfolio/main/src/assets/Cv.pdf`
+const resumeLink = `https://raw.githubusercontent.com/Soul-8789/Personal_PortFolio/main/src/assets/Cv.pdf`
 
 
 const Resume = () => {
